5c183e1819f936d683c2eef39ad317fb03ed34ad
kedarsai-1 <saikedar2003@gmail.com>
2026-02-16 18:45:04 +0530

    hi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ const Header = ()=>{
 const logoUrl = `${BASE_URL}/images/Amazon-Logo.png`;
     return(
         <div className="flex justify-center">
-            <img className="px-auto w-[200px]" src ={logoUrl}/>
-        </div>
+          </div>
     )
 }
 export default Header;
